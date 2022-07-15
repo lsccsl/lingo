e7d880b6a892e225b292ecddfa87d9d16ac6eecc
shaochuanlin@skyunion.net <shaochuanlin@skyunion.net>
2022-07-15 11:12:51 +0800

    test

--- src/lin/make.sh
+++ src/lin/make.sh
@@ -3,4 +3,4 @@ go build -o ../../output/srv lin/srv
 go build -o ../../output/epollsrv lin/srv_epoll
 go build -o ../../output/testsrv lin/testsrv
 go build -o ../../output/testepoll lin/testepoll
-
+go build -o ../../output/test_api lin/test_api
--- src/lin/tmake.sh
+++ src/lin/tmake.sh
@@ -2,4 +2,4 @@ go mod tidy -compat=1.17
 
 go build -o ../../output/testsrv lin/testsrv
 go build -o ../../output/testepoll lin/testepoll
-
+go build -o ../../output/test_api lin/test_api
