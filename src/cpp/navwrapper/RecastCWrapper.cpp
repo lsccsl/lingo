a10e93f336847da79f67ccd8bcbfebbd50bf80cb
shaochuanlin@skyunion.net <shaochuanlin@skyunion.net>
2022-09-20 11:17:05 +0800

    test

--- src/cpp/navwrapper/RecastCWrapper.cpp
+++ src/cpp/navwrapper/RecastCWrapper.cpp
@@ -15,18 +15,6 @@ void* nav_create(const char* file_path)
 	return ins;
 }
 
-void nav_findpath1(void* ins_ptr, const float startPos[3], const float endPos[3], bool bprint)
-{
-	if (NULL == ins_ptr)
-		return;
-
-	RecastInstance* ins = static_cast<RecastInstance*>(ins_ptr);
-	if (NULL == ins)
-		return;
-
-	ins->FindPath(startPos, endPos, bprint);
-}
-
 void nav_findpath(void* ins_ptr, RecastPos* p_startPos, RecastPos* p_endPos, struct RecastPos** pos_path, int* pos_path_sz, bool bprint)
 {
 	if (NULL == ins_ptr)
--- src/cpp/navwrapper/RecastCWrapper.h
+++ src/cpp/navwrapper/RecastCWrapper.h
@@ -14,8 +14,6 @@ DLL_EXPORT void* nav_create(const char * file_path);
 DLL_EXPORT void nav_findpath(void* ins_ptr, struct RecastPos * startPos, struct RecastPos * endPos, struct RecastPos** pos_path, int* pos_path_sz, bool bprint);
 DLL_EXPORT void nav_freepath(struct RecastPos* pos_path);
 
-DLL_EXPORT void nav_findpath1(void * ins_ptr, const float startPos[3], const float endPos[3], bool bprint);
-
 #ifdef __cplusplus
 }
 #endif
