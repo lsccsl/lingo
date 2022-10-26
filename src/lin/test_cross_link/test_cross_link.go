ea4efb49e978188f25a97beb36847cc4614f1d79
shaochuanlin@skyunion.net <shaochuanlin@skyunion.net>
2022-10-26 11:55:05 +0800

    test

--- src/lin/test_cross_link/test_cross_link.go
+++ src/lin/test_cross_link/test_cross_link.go
@@ -12,10 +12,10 @@ type cross_link_inf struct {
 }
 
 func (pthis*cross_link_inf)Ntf_node_in_view(node_id int, node_id_in_view int) {
-	fmt.Println(node_id, " view ", node_id_in_view)
+	//fmt.Println(node_id, " view ", node_id_in_view)
 }
 func (pthis*cross_link_inf)Ntf_node_out_view(node_id int, node_id_out_view int) {
-	fmt.Println(node_id, " out view ", node_id_out_view)
+	//fmt.Println(node_id, " out view ", node_id_out_view)
 }
 
 
@@ -44,6 +44,8 @@ func main() {
 	var cli cross_link_inf
 	clm := lin_common.Crosslink_mgr_constructor(&cli)
 
+	// test add
+	fmt.Println("add 100 step 2")
 	for i := 2; i <= 100; i += 2 {
 		node_tmp := &cross_link_node_inf {
 			x : float32(i),
@@ -53,6 +55,7 @@ func main() {
 		clm.Crosslink_mgr_add(node_tmp)
 		//fmt.Println(clm)
 		clm.Check()
+		fmt.Print(".")
 	}
 	for i := 1; i <= 99; i += 2 {
 		node_tmp := &cross_link_node_inf {
@@ -63,10 +66,13 @@ func main() {
 		clm.Crosslink_mgr_add(node_tmp)
 		//fmt.Println(clm)
 		clm.Check()
+		fmt.Print(".")
 	}
 
+	// test rand add
+	fmt.Println("\nadd rand 1000")
 	rand.Seed(0)
-	for i := 1; i <= 100; i ++ {
+	for i := 1; i <= 1000; i ++ {
 		x := float32(rand.Int() % 100)
 		y := float32(rand.Int() % 100)
 
@@ -78,6 +84,10 @@ func main() {
 		clm.Crosslink_mgr_add(node_tmp)
 		//fmt.Println(clm)
 		clm.Check()
+		fmt.Print(".")
+		if i % 100 == 0 {
+			fmt.Println()
+		}
 	}
 
 	node0 := &cross_link_node_inf {
@@ -165,43 +175,60 @@ func main() {
 
 
 	// update pos
+	fmt.Println("\n test update pos 0-100")
 	for i := 0; i < 100; i ++ {
-		fmt.Println("\n\n\n update pos\n")
 		clm.Crosslink_mgr_update_pos(node0_id, float32(i), float32(i))
 		//fmt.Println(clm)
 		clm.Check()
+		if i % 100 == 0 {
+			fmt.Println()
+		}
+		fmt.Print(".")
 	}
-	fmt.Println(clm)
+	//fmt.Println(clm)
+	fmt.Println("\n test update pos 100-0")
 	for i := 0; i < 100; i ++ {
-		fmt.Println("\n\n\n update pos\n")
 		clm.Crosslink_mgr_update_pos(node0_id, float32(100 - i), float32(100 - i))
 		//fmt.Println(clm)
 		clm.Check()
+		if i % 100 == 0 {
+			fmt.Println()
+		}
+		fmt.Print(".")
 	}
 
 	// update pos rand 100
+	fmt.Println("\n test update pos rand 100")
 	rand.Seed(0)
-	for i := 0; i < 100; i ++ {
-		fmt.Println("\n\n\n rand update pos\n")
+	for i := 0; i < 1000; i ++ {
 		x := float32(rand.Int() % 100)
 		y := float32(rand.Int() % 100)
 		clm.Crosslink_mgr_update_pos(node0_id, x, y)
 		//fmt.Println(clm)
 		clm.Check()
+		if i % 100 == 0 {
+			fmt.Println()
+		}
+		fmt.Print(".")
 	}
 
 	// udpate pos rand 1000
+	fmt.Println("\n test update pos rand 1000")
 	rand.Seed(0)
-	for i := 0; i < 100; i ++ {
-		fmt.Println("\n\n\n rand 1000 update pos\n")
+	for i := 0; i < 1000; i ++ {
 		x := float32(rand.Int() % 1000)
 		y := float32(rand.Int() % 1000)
 		clm.Crosslink_mgr_update_pos(node0_id, x, y)
 		//fmt.Println(clm)
 		clm.Check()
+		if i % 100 == 0 {
+			fmt.Println()
+		}
+		fmt.Print(".")
 	}
 
 	// add rand 1000
+	fmt.Println("\n test add rand 1000")
 	map_del := make(map[int]int)
 	rand.Seed(0)
 	for i := 1; i <= 100; i ++ {
@@ -216,26 +243,35 @@ func main() {
 		id := clm.Crosslink_mgr_add(node_tmp)
 		//fmt.Println(clm)
 		clm.Check()
-
 		map_del[id] = id
+		if i % 100 == 0 {
+			fmt.Println()
+		}
+		fmt.Print(".")
 	}
 	fmt.Println("add", clm.Crosslink_get_node_count())
 
 	// udpate pos rand 1000
+	fmt.Println("\n test update pos rand 1000")
 	rand.Seed(0)
-	for i := 0; i < 100; i ++ {
-		fmt.Println("\n\n\n rand 1000 update pos\n")
+	for i := 0; i < 1000; i ++ {
 		x := float32(rand.Int() % 1000)
 		y := float32(rand.Int() % 1000)
 		clm.Crosslink_mgr_update_pos(node0_id, x, y)
 		//fmt.Println(clm)
 		clm.Check()
+		if i % 100 == 0 {
+			fmt.Println()
+		}
+		fmt.Print(".")
 	}
 
 	// del
+	fmt.Println("\n test del")
 	for k, _ := range map_del {
 		clm.Crosslink_mgr_del(k)
 		clm.Check()
+		fmt.Print(".")
 	}
 
 	fmt.Println("delete", clm.Crosslink_get_node_count())
